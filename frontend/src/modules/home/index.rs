aeba6c240f4625ae468cc6f22890dfdb6202a896
sciretta <sciretta28@gmail.com>
2022-10-22 14:13:46 -0400

    request tasks to the server

--- frontend/src/modules/home/index.rs
+++ frontend/src/modules/home/index.rs
@@ -1,10 +1,16 @@
-use yew::prelude::*;
-use crate::{Task, modules::shared::components::Checkbox, fake::generate_fake_tasks};
+use crate::{
+    // fake::generate_fake_tasks,
+    modules::shared::components::Checkbox,
+    Task,
+};
 use gloo::{self, console::log};
+use reqwasm::http::Request;
+use serde_json;
+use yew::prelude::*;
 
 struct DayCard {
-    day:String,
-    tasks: Vec<Task>
+    day: String,
+    tasks: Vec<Task>,
 }
 
 impl DayCard {
@@ -14,7 +20,7 @@ impl DayCard {
         html! {
             <div class="mdc-card p-6 mb-1 w-full">
                 <div class="text-xl font-bold" >{self.day.clone()}</div>
-             
+
                   { tasks.into_iter().map(|task|{
                     html!{
                     <div class="flex items-center ">
@@ -29,9 +35,45 @@ impl DayCard {
 
 #[function_component(Home)]
 pub fn home() -> Html {
-    let sorted_tasks = Task::sort_tasks_by_day(&generate_fake_tasks(10)).into_iter().map(|group|DayCard{day:group.0.clone(),tasks:group.1.clone()});
+    let tasks = use_state(|| vec![]);
 
-    html!{
+    {
+        let tasks = tasks.clone();
+        use_effect_with_deps(
+            move |_| {
+                wasm_bindgen_futures::spawn_local(async move {
+                    let response = Request::get("http://localhost:8000/api/get_tasks")
+                        .send()
+                        .await
+                        .unwrap()
+                        .json::<Vec<Task>>()
+                        .await
+                        .unwrap();
+
+                    log!(serde_json::to_string_pretty(&response).unwrap()); // DELETE THIS
+                    tasks.set(response)
+                });
+                || ()
+            },
+            (),
+        );
+    }
+
+    // let sorted_tasks = Task::sort_tasks_by_day(&generate_fake_tasks(10))
+    //     .into_iter()
+    //     .map(|group| DayCard {
+    //         day: group.0.clone(),
+    //         tasks: group.1.clone(),
+    //     });
+
+    let sorted_tasks = Task::sort_tasks_by_day(&tasks)
+        .into_iter()
+        .map(|group| DayCard {
+            day: group.0.clone(),
+            tasks: group.1.clone(),
+        });
+
+    html! {
         <div class="view-container">
             { sorted_tasks.map(|card|card.view()).collect::<Html>() }
         </div>
